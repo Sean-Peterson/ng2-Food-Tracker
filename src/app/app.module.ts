96d547c010cb44d83562cf148fa24200f6a67573
Sean Peterson <SeanPeterson11@gmail.com>
2017-03-31 11:44:02 -0700

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+
 
 import { AppComponent } from './app.component';
 import { NewFoodComponent } from './new-food/new-food.component';
@@ -20,7 +22,8 @@ import { CaloriePipe } from './calorie.pipe';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
